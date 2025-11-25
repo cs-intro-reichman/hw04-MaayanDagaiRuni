25094fa88748f20ef2c0b37c0a021850f6c8304e
MaayanDagaiRuni <maayan.dagai@post.runi.ac.il>
2025-11-26 00:33:07 +0200

    ADD

--- Primes.java
+++ Primes.java
@@ -25,7 +25,7 @@ public class Primes {
 
         int count = 0;
         int p = 2;
-
+        System.out.println("Prime numbers up to " + n + ":");
         while(p<=n){
         if(elements[p] == true){
         System.out.println(p);
@@ -33,7 +33,7 @@ public class Primes {
         }
         p++;
         }
-        System.out.println("There are " + count + " primes between 2 and " + n + ". (" + (int) (((double) count/( n)) * 100) + "% are primes)");
+        System.out.println("There are " + count + " primes between 2 and " + n + " (" + (int) (((double) count/(n-1)) * 100) + "% are primes)");
 
     }
 
