c5fbe5b589536083c7dc96ef9bc331a765612a5e
MaayanDagaiRuni <maayan.dagai@post.runi.ac.il>
2025-11-25 12:41:15 +0200

    Primes

--- Primes.java
+++ Primes.java
@@ -1,5 +1,41 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        boolean[] elements = new boolean[n + 1];
+        int i = 2;
+
+        while(i < n+1){
+            elements[i] = true;
+            i++;
+        }
+        int j = 2;
+
+        while (j*j <= n){
+            if(elements[j] == true){
+                int k = j * j;
+                while (k <= n){
+                    elements[k] = false; 
+                    k += j;
+                }
+
+            }
+                j++;
+
+         }
+
+        int count = 0;
+        int p = 2;
+
+        while(p<=n){
+        if(elements[p] == true){
+        System.out.println(p);
+            count++;
+        }
+        p++;
+        }
+        System.out.println("There are " + count + " primes between 2 and " + n + ". (" + (int) (((double) count/( n)) * 100) + "% are primes)");
+
     }
+
+
 }
